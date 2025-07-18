5e072130e5c8ce077897b95c634178b3bb106209
Jorege_xar <azureusgr@gmail.com>
2025-07-18 19:17:42 +0300

    Νεα ροη λειτουργιας με μονο ενα σετ input fields

--- scripts.js
+++ scripts.js
@@ -1,35 +1,85 @@
 let totalBudgetLabel = document.getElementById("totalBudget");
 let totalBudgetAmount = 0;
 
-let esodoInputField = document.getElementById("esodo");
-let exodoInputField = document.getElementById("exodo");
 
-let esodoConfirmButton = document.getElementById("confirmEsodo");
+let neoEsodoBtn = document.getElementById("neoEsodo");
+let neoExodoBtn = document.getElementById("neoExodo");
+
+const transactionAmountInput = document.getElementById("transactionAmount");
+const transactionCommentInput = document.getElementById("transactionComment");
+const transConfirmButton = document.getElementById("confirmTransaction");
+
+const disabledInputFields = [transactionAmountInput, transactionCommentInput, transConfirmButton];
 
 
+let currentTransactionType;
 
-let numericInputFields = [esodoInputField, exodoInputField];
+const historyTable = document.getElementById("historyTable");
 
-function depositToBudget(amount){
-    totalBudgetAmount += amount;
-    totalBudgetLabel.innerText = totalBudgetAmount;
-}
+neoEsodoBtn.addEventListener('click',(e)=>{
+    currentTransactionType = 'esodo';
+    disabledInputFields.forEach(inputField => {
+        inputField.disabled = false;
+    });
+    neoExodoBtn.disabled = true;
 
-numericInputFields.forEach(numericField => {
-    numericField.onkeydown = (event) => {
+})
+neoExodoBtn.addEventListener('click',(e)=>{
+    currentTransactionType = 'exodo';
+    disabledInputFields.forEach(inputField => {
+        inputField.disabled = false;
+    });
+    neoEsodoBtn.disabled = true;
+
+})
+
+transactionAmountInput.onkeydown = (event) => {
     // Only allow if the e.key value is a number or if it's 'Backspace'
     if(isNaN(event.key) && event.key !== 'Backspace') {
         event.preventDefault();
     }
     };
-});
+
+
+function depositToBudget(amount){
+    totalBudgetAmount += amount;
+    totalBudgetLabel.innerText = totalBudgetAmount;
+}
+function withdrawFromBudget(amount){
+    totalBudgetAmount -= amount;
+    totalBudgetLabel.innerText = totalBudgetAmount;
+}
+function setDefaultState(){
+    transactionAmountInput.value = "";
+    transactionCommentInput.value = "";
+    disabledInputFields.forEach(inputField => {
+        inputField.disabled = true;
+    });
+    neoEsodoBtn.disabled = false;
+    neoExodoBtn.disabled = false;
+    
+
+}
+
+    
 
 
 
-esodoConfirmButton.addEventListener("click", (event)=>{
-    let transactionAmount = esodoInputField.valueAsNumber;
-    depositToBudget(transactionAmount);
-    console.log(transactionAmount);
+
+transConfirmButton.addEventListener("click", (event)=>{
+    let transactionAmount = transactionAmountInput.valueAsNumber;
+    if (!transactionAmount){
+        return;
+    }
+    if(currentTransactionType == "esodo"){
+        depositToBudget(transactionAmount);
+    }
+    else{
+        withdrawFromBudget(transactionAmount);
+    }
+    console.log(transactionCommentInput);
+    setDefaultState();
+    
 })
 
 
