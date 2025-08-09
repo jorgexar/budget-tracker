350657baee8dba6c6b207d68e0e8768994c776ac
Jorege_xar <azureusgr@gmail.com>
2025-08-09 13:42:58 +0300

    Nothing to see here

--- scripts.js
+++ scripts.js
@@ -1,11 +1,6 @@
 let totalBudgetLabel = document.getElementById("totalBudget");
 let totalBudgetAmount = 0;
 
-const date = new Date();
-const day = date.getDate();
-const month = date.getMonth() +1;
-const year = date.getFullYear();
-const today = `${day}/${month}/${year}`;
 
 let neoEsodoBtn = document.getElementById("neoEsodo");
 let neoExodoBtn = document.getElementById("neoExodo");
@@ -16,14 +11,17 @@ const transConfirmButton = document.getElementById("confirmTransaction");
 
 const disabledInputFields = [transactionAmountInput, transactionCommentInput, transConfirmButton];
 
-// const historyTable = document.getElementById("historyTable")
-// const historyTableLogs = document.getElementById("historyTableLogs");
 const styledInput = document.querySelectorAll(".transInput");
 let currentTransactionType;
-// let logsList = [];
+
+function getTodayDate() {
+    const date = new Date();
+    return `${date.getDate()}/${date.getMonth()+1}/${date.getFullYear()}`;
+}
 function depositToBudget(amount){
     totalBudgetAmount += amount;
     totalBudgetLabel.innerText = makeNumberReadable(totalBudgetAmount);
+   
     if(totalBudgetAmount > 0){
         totalBudgetLabel.classList.remove('expense');
     }
@@ -47,42 +45,7 @@ function setDefaultState(){
     
 }
 
-
-// function createLogEntry(amount, description, date, mark, expense){
-    
-//     let transLog = {
-//         t_mark: mark,
-//         t_amount: makeNumberReadable(amount),
-//         t_desc: description,
-//         t_date : date,
-//         t_expense : expense,
-//         printLog : function(){
-//              let row = historyTableLogs.insertRow(0);
-//             let amountCell = row.insertCell(0);
-//             let descriptionCell = row.insertCell(1);
-//             let dateCell = row.insertCell(2);
-//             let buttonCell = row.insertCell(3);
-            
-//             if(transLog.t_expense){
-//                 row.classList.add("expense");
-//             }
-            
-//             amountCell.innerHTML = transLog.t_mark + " " + transLog.t_amount;
-//             descriptionCell.innerHTML = transLog.t_desc;
-//             dateCell.innerHTML = transLog.t_date; 
-//             buttonCell.innerHTML = "❌";
-            
-//             buttonCell.addEventListener("click",(e)=>{
-//                 buttonCell.parentNode.parentNode.removeChild(buttonCell.parentNode);
-//             })
-//         }
-//     }
-//     logsList.push(transLog);
-//     console.log(logsList); 
-//     transLog.printLog();
-    
-    
-// }    
+   
 function createCard(t_amount, t_desc, t_date, isExpense){
     const pg = document.getElementById('pg');
     const card = document.createElement('div')
@@ -108,12 +71,11 @@ function createCard(t_amount, t_desc, t_date, isExpense){
     desc.id = 'desc';
     date.id = 'date';
     if(isExpense){
-        formattedNumber = "-"+formattedNumber;
+        amount.innerText = "-"+ formattedNumber;
         card.classList.add("expense");
     }else{
-        formattedNumber = "+"+formattedNumber;
+        amount.innerText = "+"+ formattedNumber;
     }
-    amount.innerText= formattedNumber;
     delButton.innerText = "❌"
     desc.innerText = t_desc;
     date.innerText = t_date;
@@ -128,7 +90,7 @@ function createCard(t_amount, t_desc, t_date, isExpense){
     cardMid.appendChild(date);
     delButton.addEventListener("click",(e)=>{
             if (confirm("Are you sure you want to delete this log?") == true) {
-            delButton.parentNode.parentNode.parentNode.removeChild(delButton.parentNode.parentNode);
+            delButton.closest('.card').remove();
             }                
             })
     pg.appendChild(card);
@@ -138,16 +100,8 @@ function createCard(t_amount, t_desc, t_date, isExpense){
 }
 // createCard();
 function makeNumberReadable(amount){
-        let amountArr = amount.toString().split("");
-    const dotTimes = Math.floor(amountArr.length / 3);
-    let startingLen = amountArr.length -1;
-    
-    for(let i=1; i<=dotTimes; i++){
-        amountArr[startingLen - (3 * i)]+=".";
-        
-    }
-    amountArr = amountArr.join("");
-    return(amountArr);
+    amount = amount.toLocaleString('el-GR', {style: "currency", currency: "EUR"})
+    return(amount);
     
 }
 
@@ -190,7 +144,7 @@ transConfirmButton.addEventListener("click", (event)=>{
     let transAmount = transactionAmountInput.valueAsNumber;
     let transDescription = transactionCommentInput.value;
     let mark;
-    let transDate = today;
+    let transDate = getTodayDate();
     let expense = false;
     if (!transAmount){
         return;
